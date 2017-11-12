4f29795fb58b23be9c429c4e001d2d40ea8626cf
alaw989 <alaw989@gmail.com>
2017-11-12 17:36:49 -0500

    response changes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
       movieData: [
         {
           backdrop_path: "/gMCKSic0my5n5ovqJU7fjMC0tD.jpg",
-          original_title: "Next Fridays",
+          original_title: "Next Friday",
           overview:
             "Ice Cube returns as Craig Jones, a streetwise man from South Central Los Angeles who has a knack for getting into trouble. This time out, Craig is still trying to outsmart neighborhood bully Debo (Tommy 'Tiny' Lister Jr.); after Craig gets the better of Debo in a fist fight, Debo is determined to flatten Craig in a rematch. Looking to stay out of Debo's way, Craig's dad decides that it would be a good idea for Craig to hide out with his Uncle Elroy and cousin Day-Day in Rancho Cucamonga... but trouble seems to find him there also.",
           popularity: 8.689173,
