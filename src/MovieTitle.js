6afc84a61665a40749866e94181e7093d78481ea
alaw989 <alaw989@gmail.com>
2017-11-14 22:03:45 -0500

    added styling

--- src/MovieTitle.js
+++ src/MovieTitle.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const MovieTitle = (props) => {
 
     let title;
-    props.movie[0].title == null ? title = "Next Fridays" : title = props.movie[0].title;
+    props.movie[0].title == null ? title = "Next Friday" : title = props.movie[0].title;
    
     return <h1 className="MovieTitle-text">{title.toUpperCase()}</h1>
     
